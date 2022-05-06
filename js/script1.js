ffce1d32f17de55e69c9f51ec77275b324ffbc51
illya bondar <1816579@gmail.com>
2022-05-06 20:55:41 +0300

    promise microtask demo

--- js/script1.js
+++ js/script1.js
@@ -124,3 +124,13 @@ const heapSort = (array) => {
 const inputArray = [23, 1, 34, 15, 165, 312, 59, 12, 1023, 22];
 console.log(inputArray);
 console.log(heapSort(inputArray));
+
+//====================================================================
+
+setTimeout(a => { console.log("A") }, 0);
+
+const prom1 = new Promise(resolve => resolve("success value"));
+prom1.then(value => console.log(value));
+//promise is a microtask would log first, then timed out function executes
+
+//====================================================================
